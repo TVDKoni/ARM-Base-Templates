ffff5c076d0c1e238300fcaa3d983e53c303227f
Konrad Brunner <konrad.brunner@trivadis.com>
2017-03-24 15:51:19 +0100

    First working state

---
+++ BaseInfrastructure.ps1
@@ -0,0 +1,25 @@
+$SolutionPrefix = "ttvdsol"
+$AzureSubscriptionName = "Microsoft Azure"
+$ResourceGroupName = ($SolutionPrefix + "resg001")
+$ResourceGroupLocation = "West Europe"
+$TemplateFileUri = "https://raw.githubusercontent.com/TVDKoni/ARM-Base-Templates/master/BaseInfrastructure/baseInfrastructureDeployment.json"
+
+$TemplateParameters = @{
+    namePrefix = $SolutionPrefix
+    vpnGateway = "basic"
+}
+$ResourceGroup = @{
+    Name = $ResourceGroupName
+    Location = $ResourceGroupLocation
+    Force = $true
+}
+
+Login-AzureRmAccount
+Get-AzureRmSubscription –SubscriptionName $AzureSubscriptionName | Select-AzureRmSubscription
+New-AzureRmResourceGroup @ResourceGroup
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFileUri -TemplateParameterObject $TemplateParameters -Verbose
+
+pause
+
+
+
