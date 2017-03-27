ed3ac42c38b858ed7b743a2eb0d79bb89afa85a3
Konrad Brunner <konrad.brunner@trivadis.com>
2017-03-27 13:10:41 +0200

    Trying to switch to custom script and removing dsc

---
+++ HighPerformance/scripts/ConfigS2D.ps1
@@ -0,0 +1,19 @@
+Import-Module Storage
+
+$storagePool = New-StoragePool -FriendlyName "DataPool" `
+    -StorageSubSystemUniqueId (Get-StorageSubSystem).uniqueID `
+    -PhysicalDisks (Get-PhysicalDisk -CanPool $true)
+Start-Sleep -Seconds 20
+
+$disk = New-VirtualDisk -FriendlyName "DataDisk01" `
+-StoragePoolFriendlyName "DataPool" -Size $storagePool.Size `
+-ProvisioningType Thin -ResiliencySettingName Simple | Get-Disk
+Start-Sleep -Seconds 20
+
+Initialize-Disk -Number $disk.Number
+Start-Sleep -Seconds 20
+
+$part = New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter
+Start-Sleep -Seconds 20
+
+Format-Volume -DriveLetter $part.DriveLetter -FileSystem NTFS -NewFileSystemLabel "DataDisk01"
